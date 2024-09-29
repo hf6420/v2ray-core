0589d195da158409062980661437d316f9b0280f
hf6420 <175292968+hf6420@users.noreply.github.com>
2024-09-29 11:41:52 +0800

    Update resp.go

--- transport/internet/headers/http/resp.go
+++ transport/internet/headers/http/resp.go
@@ -5,6 +5,30 @@ var resp400 = &ResponseConfig{
 		Value: "1.1",
 	},
 	Status: &Status{
+		Code:   "400",
+		Reason: "Bad Request",
+	},
+	Header: []*Header{
+		{
+			Name:  "Connection",
+			Value: []string{"close"},
+		},
+		{
+			Name:  "Cache-Control",
+			Value: []string{"private"},
+		},
+		{
+			Name:  "Content-Length",
+			Value: []string{"0"},
+		},
+	},
+}
+
+var resp301 = &ResponseConfig{
+	Version: &Version{
+		Value: "1.1",
+	},
+	Status: &Status{
 		Code:   "301",
 		Reason: "{'code':'301','msg':'Token is null'}",
 	},
